4a06d8d59ee0c92b338fbe56101d33c5ef7b56b9
DennisWang <Dennis.june@Outlook.com>
2020-07-19 18:34:48 +0800

    第一次提交 项目构建初始化

---
+++ greendillmall-member/src/main/java/com/greendill/greendillmall/member/GreendillmallMemberApplication.java
@@ -0,0 +1,13 @@
+package com.greendill.greendillmall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreendillmallMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreendillmallMemberApplication.class, args);
+    }
+
+}
---
+++ greendillmall-member/src/test/java/com/greendill/greendillmall/member/GreendillmallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greendill.greendillmall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreendillmallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
