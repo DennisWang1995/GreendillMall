558cc251dcf42dde40829fa2df4c515dcfea8876
DennisWang <Dennis.june@Outlook.com>
2020-07-22 16:30:27 +0800

    测试网关服务

--- greendillmall-gateway/src/main/java/com/greendill/greendillmall/gateway/GreendillmallGateApplication.java
+++ greendillmall-gateway/src/main/java/com/greendill/greendillmall/gateway/GreendillmallGateApplication.java
@@ -2,11 +2,12 @@ package com.greendill.greendillmall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @EnableDiscoveryClient //开启网关的注册与发现功能
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GreendillmallGateApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/greendill/greendillmall/gateway/GreendillmallGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.greendill.greendillmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreendillmallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreendillmallGatewayApplication.class, args);
+    }
+
+}
